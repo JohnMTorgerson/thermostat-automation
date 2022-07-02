1df77b3eb8af1f69bbb1284149d97ad1c549aedf
John Torgerson <john.m.torgerson@gmail.com>
2022-07-02 12:22:16 -0500

    added logging

--- log_temp.py
+++ log_temp.py
@@ -7,7 +7,7 @@ import adafruit_dht
 import datetime
 
 
-def get_and_log() :
+def get_and_log(log=True) :
     # Initial the dht device, with data pin connected to:
     # dhtDevice = adafruit_dht.DHT11(board.D4)
 
@@ -42,29 +42,29 @@ def get_and_log() :
             print(f"{now} temp: {temperature_f}° hum: {humidity}%")
 
             # === log the values === #
+            if log == True :
+                try:
+                    # check if new value is different from last recorded value
+                    with open(data_path, "r") as f:
+                        last_line = f.readlines()[-1].split()
+                except Exception as error:
+                    last_line = [-1,-1,-1]
+                    print(f"Error: {error}")
 
-            try:
-                # check if new value is different from last recorded value
-                with open(data_path, "r") as f:
-                    last_line = f.readlines()[-1].split()
-            except Exception as error:
-                last_line = [-1,-1,-1]
-                print(f"Error: {error}")
+                try:
+                    if float(last_line[1]) != temperature_f or abs(float(last_line[2]) - humidity) > 1:
+                        #print(last_line)
+                        #print(float(last_line[1]))
+                        #print(float(last_line[2]))
+                        print("different!!!!")
 
-            try:
-                if float(last_line[1]) != temperature_f or abs(float(last_line[2]) - humidity) > 1:
-                    #print(last_line)
-                    #print(float(last_line[1]))
-                    #print(float(last_line[2]))
-                    print("different!!!!")
+                        with open(data_path, "a") as f:
+                            f.write(f"{int(now.timestamp())} {temperature_f} {humidity} ({now})\n")
 
+                except (ValueError, IndexError) as error:
+                    # if a line doesn't follow the format, (we might have added a comment), then ignore that and write a new line
                     with open(data_path, "a") as f:
                         f.write(f"{int(now.timestamp())} {temperature_f} {humidity} ({now})\n")
-
-            except (ValueError, IndexError) as error:
-                # if a line doesn't follow the format, (we might have added a comment), then ignore that and write a new line
-                with open(data_path, "a") as f:
-                    f.write(f"{int(now.timestamp())} {temperature_f} {humidity} ({now})\n")
 
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
--- thermostat.py
+++ thermostat.py
@@ -1,32 +1,64 @@
-def run() :
-    hysteresis = 3
+# print("__package__, __name__ ==", __package__, __name__)
+import logging
+# create logger
+therm_logger = logging.getLogger(f"main.{__name__}")
 
+
+def run(client=None,plugs=[]) :
+    if client == None or len(plugs) == 0 :
+        return
+
+    therm_logger.info("Running thermostat scene...")
     temp = get_current_temp()
     target = get_temp_target()
+    hysteresis = get_hysteresis()
+
 
     if temp <= target - hysteresis :
         # turn off A/C
-        pass
+        therm_logger.info(f'Temp is {temp}, {target-temp} degrees below target: TURNING A/C OFF')
+        switchAC(value="off",client=client, plugs=plugs)
 
     if temp >= target + hysteresis :
         # turn on A/C
-        pass
+        therm_logger.info(f'Temp is {temp}, {temp-target} degrees above target: TURNING A/C ON')
+        switchAC(value="on",client=client, plugs=plugs)
 
 
 def get_current_temp() :
     try :
         from . import log_temp
         values = log_temp.get_and_log()
+        therm_logger.info(f"CURRENT VALUES ==== temp_c:{values['temp_c']}, temp_f:{values['temp_f']}, humidity:{values['humidity']}")
     except ModuleNotFoundError as e :
         # if not running on raspberry pi with 'board' module, just try some test values
-        values = {'temp_c': 24, 'temp_f': 75.2, 'humidity': 32}
+        values = {'temp_c': 24, 'temp_f': 70, 'humidity': 32}
+        therm_logger.error(f"Error: not running on raspberry pi, using test values: temp_c:{values['temp_c']}, temp_f:{values['temp_f']}, humidity:{values['humidity']}")
+    except Exception as e :
+        therm_logger.error(f"Other error getting temp/humidity values: {e}")
+        raise e
 
-    print(values)
     return values["temp_f"]
 
 def get_temp_target() :
-    return 75
+    target = 75
+    therm_logger.info(f"Target temp is {target} degrees F")
+    return target
 
+def get_hysteresis() :
+    hyst = 3
+    therm_logger.info(f"Hysteresis value is +/- {hyst} degrees F")
+    return hyst
+
+
+def switchAC(value="",client=None, plugs=[]) :
+    therm_logger.debug(f"Turning {value} plugs:")
+    for plug in plugs :
+        therm_logger.debug(plug.nickname)
+        if value == "off" :
+            client.plugs.turn_off(device_mac=plug.mac, device_model=plug.product.model)
+        elif value == "on" :
+            client.plugs.turn_on(device_mac=plug.mac, device_model=plug.product.model)
 
 if __name__ == "__main__" :
     run()
